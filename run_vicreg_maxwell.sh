52928c9642734728e04312f614e07294e5b96a81
AlzbetaM <alzbeta.manova77@gmail.com>
2023-05-04 22:18:52 +0100

    fix config

--- run_vicreg_maxwell.sh
+++ run_vicreg_maxwell.sh
@@ -4,13 +4,13 @@
 #SBATCH --mem=64G # memory pool for all cores
 #SBATCH -o slurm.%j.out # STDOUT
 #SBATCH -e slurm.%j.err # STDERR
-#SBATCH --ntasks-per-node=2
-#SBATCH --gres=gpu:2
-#SBATCH --partition=spot-gpu
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=u25am19@abdn.ac.uk
 #SBATCH --signal=SIGUSR1@90
-#SBATCH --nodelist=cgpu003
+#SBATCH --nodelist=gpu03
 
 module load anaconda3
 source activate pt
@@ -18,5 +18,9 @@ source activate pt
 nvidia-smi
 
 rm logfiles.txt
-srun python src/pretrain.py -c=/uoa/home/u25am19/sharedscratch/VICReg/config_maxwell.conf --num_nodes=1 --devices=2
-srun python src/finetune.py -c=/uoa/home/u25am19/sharedscratch/VICReg/config_maxwell.conf --num_nodes=1 --devices=2
+srun python src/pretrain.py -c=/uoa/home/u25am19/sharedscratch/VICReg/config_maxwell.conf
+srun python src/finetune.py -c=/uoa/home/u25am19/sharedscratch/VICReg/config_maxwell.conf
+
+rm logfiles.txt
+srun python src/pretrain.py -c=/uoa/home/u25am19/sharedscratch/VICReg/config_maxwell.conf --projection=stacked
+srun python src/finetune.py -c=/uoa/home/u25am19/sharedscratch/VICReg/config_maxwell.conf --projection=stacked
