6994ca8e4ac48ce992fb480e7c90fbd06d9e0b96
AlzbetaM <alzbeta.manova77@gmail.com>
2023-05-06 23:52:12 +0100

    fixxxxx

--- src/lin_eval.py
+++ src/lin_eval.py
@@ -103,7 +103,7 @@ class SSLLinearEval(pl.LightningModule):
     def encode(self, x):
         with torch.no_grad():
             if self.stacked:
-                i, j = 16
+                i = j = 16
                 if self.hparams.projection == "none" or self.hparams.projection == "stacked":
                     j = 32
                 s, _ = self.enc1(x)
