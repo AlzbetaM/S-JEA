0db7d4e7c75483aa77085ebd71e00748cce8811a
AlzbetaM <alzbeta.manova77@gmail.com>
2023-05-10 00:16:40 +0100

    trial model

--- src/infer.py
+++ src/infer.py
@@ -1,10 +1,12 @@
 import pytorch_lightning as pl
+import torch
 from configargparse import ArgumentParser
 import json
+from neptune.integrations.pytorch_lightning import NeptuneLogger
 
+from VICReg.src.utils import FTPrintingCallback
 from dataloaders.handlers import get_dm
 from lin_eval import SSLLinearEval
-from dataloaders.cifar10 import Cifar10_DataModule
 
 
 def get_args(d):
@@ -28,13 +30,24 @@ def infer():
     args.data_dir = "../" + args.data_dir
     args.strategy = None
 
-    trainer = pl.Trainer()
-    dm, ft_dm, args = get_dm(args)
+    trainer = pl.Trainer.from_argparse_args \
+        (args, max_epochs=args.ft_epochs,
+         deterministic=True,
+         enable_checkpointing=False,
+         fast_dev_run=False,
+         sync_batchnorm=False,
+         accelerator='cpu',
+         check_val_every_n_epoch=args.ft_val_freq,
+         accumulate_grad_batches=args.ft_accumulate_grad_batches)
+
+    _, ft_dm, args = get_dm(args)
     checkpoint_path = "C:/Users/alzbe/Documents/VICReg2/VICReg/model/best_epoch.ckpt"
 
-    model = SSLLinearEval.load_from_checkpoint(checkpoint_path)
+    model = SSLLinearEval.load_from_checkpoint(checkpoint_path, map_location=torch.device('cpu'))
+    model.freeze()
     # Perform inference
-    trainer.predict(model, ft_dm)
+    pred = model(ft_dm)
+    print(pred)
 
 
 if __name__ == '__main__':
